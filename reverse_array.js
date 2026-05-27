d68b605043fbeb73cd1d166dcaf6f5375272bc6c
Mouryaditya Sunkara <155514226+adityamourya42@users.noreply.github.com>
2026-05-27 23:55:29 +0530

    Add files via upload

---
+++ reverse_array.js
@@ -0,0 +1,10 @@
+let str = "hello";
+let charArray = str.split("");
+console.log(charArray);
+let reversedArray = charArray.reverse();
+console.log(reversedArray);
+
+let sen = "Hello World";
+let wordsArray = sen.split(" ");
+console.log(wordsArray);
+ 
