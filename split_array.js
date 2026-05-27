03a55ea4b0a1f5ab2b5c2b76c9e566117031aa1f
Mouryaditya Sunkara <155514226+adityamourya42@users.noreply.github.com>
2026-05-27 17:11:11 +0530

    Add files via upload

---
+++ split_array.js
@@ -0,0 +1,15 @@
+let str = "hello";
+let charArray = str.split("");
+console.log(charArray);
+
+let sen = "Hello World";
+let wordsArray = sen.split(" ");
+console.log(wordsArray);
+ 
+let sentence = "JavaScript is a awesome language";
+let wordArray = sentence.split(" ");
+console.log(wordArray);
+
+let dash = "2026-05-26";
+let dateArray = dash.split("-");
+console.log(dateArray);
